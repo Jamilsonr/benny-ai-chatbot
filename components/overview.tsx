020e7437fd5fa77f8fcef07f350d67c58e3f4ddf
Jamilson Rocha <81302730+Jamilsonr@users.noreply.github.com>
2025-04-03 16:42:16 -0100

    Update overview.tsx

--- components/overview.tsx
+++ components/overview.tsx
@@ -10,9 +10,6 @@ export const Overview = () => {
       exit={{ opacity: 0, scale: 0.98 }}
       transition={{ delay: 0.5 }}
     >
-      <div className="rounded-xl p-6 flex flex-col items-center justify-center leading-relaxed text-center max-w-xl">
-        <p>Em que posso ajudar?</p>
-      </div>
     </motion.div>
   );
 };
